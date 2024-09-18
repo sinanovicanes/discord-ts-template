15a7d664d45cca2db4eb216ca19c9562c0d66bc2
sinanovicanes <sinanovicanes01@gmail.com>
2024-09-18 19:42:27 +0300

    feat: Update logger to save log files if passed in options

--- libs/common/classes/logger.ts
+++ libs/common/classes/logger.ts
@@ -1,4 +1,10 @@
 import chalk from "chalk";
+import { createWriteStream, WriteStream } from "fs";
+import { join } from "path";
+import { createDirectoryIfNotExists } from "../utils";
+
+const DEFAULT_SAVE_TIMEOUT = 10 * 1000;
+const LOGS_DIRECTORY = "./logs";
 
 enum LogKind {
   LOG = "LOG",
@@ -12,24 +18,103 @@ enum LogKind {
 interface Log {
   kind: LogKind;
   message: string;
-  timestamp: number;
+  timestamp: Date;
+}
+
+interface LoggerOptions {
+  saveLogs?: boolean;
+  saveTimeout?: number;
 }
 
 export class Logger {
+  private readonly path = join(process.cwd(), LOGS_DIRECTORY, `${this.name}.log`);
   private logs: Log[] = [];
+  private writeStream: WriteStream | null = null;
+  private writeLogTimeout: NodeJS.Timeout | null = null;
+  private closeStreamTimeout: NodeJS.Timeout | null = null;
 
-  constructor(private readonly name: string) {}
+  constructor(
+    private readonly name: string,
+    private readonly options: LoggerOptions = {}
+  ) {}
+
+  private initializeWriteStream() {
+    // We don't need to create a write stream if we already have one
+    if (this.writeStream) return;
+
+    // Make sure the directory exists
+    createDirectoryIfNotExists(join(process.cwd(), LOGS_DIRECTORY));
+    this.writeStream = createWriteStream(this.path, { flags: "a+" });
+  }
+
+  private closeStream() {
+    this.clearCloseStreamTimeout();
+    if (!this.writeStream) return;
+    this.writeStream.close();
+    this.writeStream = null;
+  }
+
+  private clearCloseStreamTimeout() {
+    if (!this.closeStreamTimeout) return;
+    clearTimeout(this.closeStreamTimeout);
+    this.closeStreamTimeout = null;
+  }
+
+  private scheduleStreamClosure() {
+    if (!this.writeStream) return;
+
+    this.clearCloseStreamTimeout();
+    this.closeStreamTimeout = setTimeout(() => {
+      this.closeStream();
+    }, 1000);
+  }
+
+  private writeLogs() {
+    if (this.writeLogTimeout) return;
+    if (!this.options.saveLogs) return;
+
+    this.initializeWriteStream();
+    this.clearCloseStreamTimeout();
+
+    this.writeLogTimeout = setTimeout(() => {
+      const logsToWrite = this.logs;
+
+      const logString =
+        logsToWrite
+          .map(
+            log => `[${log.timestamp.toISOString()}] ${LogKind[log.kind]}: ${log.message}`
+          )
+          .join("\n") + "\n";
+
+      try {
+        this.writeStream!.write(logString);
+      } catch (e) {
+        return this.processLog(LogKind.ERROR, `Failed to write logs to file: ${e}`);
+      } finally {
+        this.writeLogTimeout = null;
+        this.scheduleStreamClosure();
+      }
+
+      // Clear the logs if we successfully wrote them
+      this.logs = [];
+    }, this.options.saveTimeout || DEFAULT_SAVE_TIMEOUT);
+  }
+
+  private saveLog(kind: LogKind, message: string, date: Date = new Date()) {
+    const log: Log = {
+      kind,
+      message,
+      timestamp: date
+    };
+
+    this.logs.push(log);
+    this.writeLogs();
+  }
 
   private processLog(kind: LogKind, message: string) {
     const date = new Date();
 
-    const log: Log = {
-      kind,
-      message,
-      timestamp: date.getTime()
-    };
-
-    this.logs.push(log);
+    this.saveLog(kind, message);
 
     const name = chalk.bold(chalk.yellowBright(`[${this.name}]`));
     const prefix = `${chalk.white(date.toLocaleString())} ${LogKind[kind]} ${name}`;
--- libs/common/utils/helpers.ts
+++ libs/common/utils/helpers.ts
@@ -1,3 +1,11 @@
+import { existsSync, mkdirSync } from "fs";
+
 export function pluralify(singular: string, plural: string, count: number): string {
   return count === 1 ? singular : plural;
+}
+
+export function createDirectoryIfNotExists(path: string): void {
+  if (!existsSync(path)) {
+    mkdirSync(path, { recursive: true });
+  }
 }
